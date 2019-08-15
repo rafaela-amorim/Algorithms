ae95ee3fa79e0d19ac1612fccff8e73cc533f5d7
Rafaela <rafaela.silva@ccc.ufcg.edu.br>
2019-08-15 16:12:41 -0300

    update on sgtree with lazy propagation, now it's complete

--- sgTree_lazy_propagation.cpp
+++ sgTree_lazy_propagation.cpp
@@ -34,7 +34,53 @@ void build(int id, int left, int right) {
     }
 }
 
-int query()
+/* Since the operation being used in this segment tree is the sum
+    The neutral element is 0, because any number plus zero is the number
+*/
+void propaga(int id, int left, int right) {
+    if (lazy[id] != 0) {
+        /*  In the sum seg tree, the upper nodes keep the values of the TOTAL SUM 
+            in a specific range in the array.
+            That's why we sum the current range (right - left) + 1 and multiply it
+            by the value we want which is the one kept in the lazy tree
+        */
+        sgtree[id] += lazy[id] * ((right - left) + 1);  
+
+        if (left != right) {    // if not leaf
+            lazy[id*2] = lazy[id];
+            lazy[id*2 + 1] = lazy[id];  // the actual propagation
+        }
+    }
+}
+
+int query(int id, int left, int right, int x, int y) {
+    if (x <= left && right <= y) {
+        return sgtree[id];
+    } else if (right < x || left > y) {
+        return 0;   // neutral element
+    } else {
+        int middle = (left + right) / 2;
+
+        return query(id*2,left,middle,x,y) + query(id*2 + 1,middle+1,right,x,y);    // returns the sum
+    }
+}
+
+/* The update happens in range */
+void update(int id, int left, int right, int x, int y, int value) {
+    if (x <= left && right <= y) {
+        lazy[id] = value;           // update the lazy tree before propagating the "cache"
+        propaga(id, left, right);
+    } else if (right < x || left > y) {
+        propaga(id, left, right);
+    } else {
+        propaga(id, left, right);
+
+        int middle = (left + right) / 2;
+
+        update(id*2, left, middle, x, y, value);
+        update(id*2 + 1, middle + 1, right, x, y, value);
+    }
+}
 
 // ---------------------------------
 
